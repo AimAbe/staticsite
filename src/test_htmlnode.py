ed831b0fa8e43fdde920a7a3f529dd3919f3f89c
AimAbe <aimen.aberra@gmail.com>
2025-05-25 19:24:44 +0000

    Added HTML Node and Unit Tests for it

---
+++ src/test_htmlnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+    """ def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            [],
+        )
+        self.assertEqual(
+            node.props,
+            {},
+        ) """
+if __name__ == "__main__":
+    unittest.main()  
